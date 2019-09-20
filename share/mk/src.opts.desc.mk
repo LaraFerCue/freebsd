368de0055819b94df0dd9b94d451054e86ca58a0
Lara Fernandez Cueto <r.fernandez-cueto@bally-wulff.de>
2019-09-20 14:21:59 +0200

    Adding a target to present a menu with different categories

---
+++ share/mk/src.opts.desc.mk
@@ -0,0 +1,28 @@
+# $FreeBSD$
+#
+# Descriptions for the categories and knobs of the options available for
+# configuration.
+#
+
+# Categories and descriptions
+CATEGORIES= \
+	ADMIN \
+	DEVEL \
+	DOCS \
+	DRIVERS \
+	EMULATION \
+	NETWORK \
+	OTHER \
+	PROGRAMS \
+	SECURITY
+
+ADMIN_DESC=	"Administration programs"
+DEVEL_DESC=	"Building options, compilers and debugging information."
+DOCS_DESC=	"Documentation options"
+DRIVERS_DESC=	"Kernel modules or options relative to devices."
+EMULATION_DESC=	"Emulation/virtualitation options."
+NETWORK_DESC=	"Programs or kernel options/modules to build."
+OTHER_DESC=	"Other options to configure."
+PROGRAMS_DESC=	"Non-administrative programs to build."
+SECURITY_DESC=	"Security features to include in the build."
+
